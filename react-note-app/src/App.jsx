08c0cd0fa3379679536e45ff32e3b7dd4e6381f5
Kariuki_Ken <irungukenneth433@gmail.com>
2024-09-16 22:15:55 +0300

    changes

--- react-note-app/src/App.jsx
+++ react-note-app/src/App.jsx
@@ -1,15 +1,13 @@
-import React from 'react'
-import NavBar from './components/NavBar'
-import Filter from './components/Filter'
-import NoteCardContainer from './components/NoteCardContainer'
+import HomePage from './pages/HomePage'
+import { createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom'
 
 function App() {
-  return (
-    <>
-    {/* <NavBar /> */}
-  
-    </>
-  )
+
+  const router = createBrowserRouter(createRoutesFromElements(
+    <Route index element={<HomePage />} />
+  ))
+
+  return <RouterProvider router={router} />
 }
 
 export default App
