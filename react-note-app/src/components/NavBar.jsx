d60fd99b1844627a5f41b0175ed36fb8d00d7e1c
Kariuki_Ken <irungukenneth433@gmail.com>
2024-09-16 21:24:54 +0300

    changes

--- react-note-app/src/components/NavBar.jsx
+++ react-note-app/src/components/NavBar.jsx
@@ -2,7 +2,40 @@ import React from 'react'
 
 function NavBar() {
   return (
-    <div>NavBar</div>
+    <nav className="navbar bg-body-tertiary py-50" style={{ padding: "20px" }}>
+      <div className="container d-flex justify-content-around">
+        <Link className="navbar-brand" to="/">
+          <h4 style={{ fontWeight: "bold" }}>Notey</h4>
+        </Link>
+        <div className="d-flex">
+          <div
+            className="input-group input-group-sm"
+            style={{ width: "500px", height: "40px" }}
+          >
+            <input
+              className="form-control"
+              type="search"
+              placeholder="Search"
+              aria-label="Search"
+            />
+            <button className="btn btn-outline-success" type="submit">
+              Search
+            </button>
+          </div>
+          {/* <button className="btn btn-outline-primary btn-md" type="button">Add</button> */}
+        </div>
+
+        <Link to="/add-notes"  style={{ textDecoration: "none" }}>
+          <button
+            className="btn btn-outline-primary btn-md"
+            type="button"
+           
+          >
+            <FaSquarePlus /> Add Notes
+          </button>
+        </Link>
+      </div>
+    </nav>
   )
 }
 
