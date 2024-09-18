89a49eabadaf8ab27773e2db2746926df404f54b
Kariuki_Ken <irungukenneth433@gmail.com>
2024-09-19 01:21:51 +0300

    "Added form elements to EditNotePage component"

--- react-note-app/src/pages/EditNotePage.jsx
+++ react-note-app/src/pages/EditNotePage.jsx
@@ -2,7 +2,49 @@ import React from 'react'
 
 function EditNotePage() {
   return (
-    <div>EditNotePage</div>
+    <form>
+      <h5>Add New Note</h5>
+      <div className="mb-3">
+        <label htmlFor="exampleFormControlInput1" className="form-label">
+          Title
+        </label>
+        <input
+          type="email"
+          className="form-control"
+          id="exampleFormControlInput1"
+          placeholder="Enter note's title"
+        />
+      </div>
+
+      <div className="mb-3">
+        <label htmlFor="exampleFormControlTextarea1" className="form-label">
+          Content
+        </label>
+        <textarea
+          className="form-control"
+          id="exampleFormControlTextarea1"
+          rows={4}
+          placeholder="Enter note's content"
+        ></textarea>
+      </div>
+
+      <div className="mb-3">
+      <label htmlFor="exampleFormControlTextarea1" className="form-label">
+          Note's category
+        </label>
+      <select className="form-select" aria-label="Default select example" style={{height: "40px"}}>
+          <option selected>Pick a category</option>
+          <option value="1">Business</option>
+          <option value="2">Personal</option>
+          <option value="3">Important</option>
+        </select>
+      </div>
+
+        
+
+
+      <button className="btn btn-primary d-flex justify-content-center" style={{width:"100%"}}>Add Note</button>
+    </form>
   )
 }
 
