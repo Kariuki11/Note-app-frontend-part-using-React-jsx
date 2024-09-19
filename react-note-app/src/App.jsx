e120b569b312c41c125b3842d5d723fb9d161275
Kariuki_Ken <irungukenneth433@gmail.com>
2024-09-20 01:17:34 +0300

    changes

--- react-note-app/src/App.jsx
+++ react-note-app/src/App.jsx
@@ -25,7 +25,7 @@ function App() {
 
   const router = createBrowserRouter(createRoutesFromElements(
     <Route path="/" element={<MainLayout/>}>
-      <Route index element={<HomePage />} />
+      <Route index element={<HomePage notes={notes} />} />
       <Route path="/add-note" element={<AddNotePage />} />
       <Route path="/edit-note" element={<EditNotePage />} />
       <Route path="/note-detail" element={<NoteDetailPage />}/>
