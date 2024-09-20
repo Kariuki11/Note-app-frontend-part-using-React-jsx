cf012cd4b330420683dec69f5ba2039a1aac9144
Kariuki_Ken <irungukenneth433@gmail.com>
2024-09-20 17:20:18 +0300

    changes

--- react-note-app/src/pages/NoteDetailPage.jsx
+++ react-note-app/src/pages/NoteDetailPage.jsx
@@ -4,6 +4,7 @@ import { FiEdit } from "react-icons/fi";
 import { Link, useParams } from 'react-router-dom';
 import "./NoteDetailPage.css"
 import axios from "axios"
+import { FormatDate } from '../components/FormatDate';
 
 function NoteDetailPage() {
 
@@ -24,10 +25,10 @@ function NoteDetailPage() {
 
   return (
     <div className="note-container">
-    <h3 className="title">Note app Project React- Frontend</h3>
+    <h3 className="title">{note.title}</h3>
     <span className="d-flex justify-content-center">
-    <p className="note-date font-12 text-muted me-5"> created: 11 March 2009</p>
-    <p className="note-date font-12 text-muted me-5">last updated: 11 March 2009</p>
+    <p className="note-date font-12 text-muted me-5">Created At: {FormatDate(note.created)}</p>
+    <p className="note-date font-12 text-muted me-5">Last Updated: {FormatDate(note.created)}</p>
     </span>
     <span className="button-group">
       <Link to="/edit-note">
@@ -36,19 +37,7 @@ function NoteDetailPage() {
       <button className="btn btn-danger"><BiSolidTrashAlt /><span>Delete</span></button>
     </span>
     <p className="description">
-      Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-      accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab
-      illo inventore veritatis et quasi architecto beatae vitae dicta sunt
-      explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut
-      odit aut fugit, sed quia consequuntur magni dolores eos qui ratione
-      voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum
-      quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam
-      eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat
-      voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam
-      corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur?
-      Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse
-      quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo
-      voluptas nulla pariatur?
+      {note.body}
     </p>
 
 
