bad6592a3fe0b00c8e8ad083f8ddba9a7fdd194d
Kariuki_Ken <irungukenneth433@gmail.com>
2024-09-24 22:46:46 +0300

    changes

--- react-note-app/src/components/Modal.jsx
+++ react-note-app/src/components/Modal.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 
+
 const Modal() {
   return (
     <div className="c-modal-overlay">
@@ -7,6 +8,11 @@ const Modal() {
             <button className="close-button">x</button>
             <div className="c-modal-content">
                 <h2>Delete Notes</h2>
+                <p>Are you sure you want to delete this note?</p>
+                <span className="d-flex justify-content-center">
+                    <button className="btn btn-danger me-3">Delete</button>
+                    <button className="btn btn-primary">Cancel</button>
+                </span>
             </div>
         </div>
     </div>
